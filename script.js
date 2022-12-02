3b6273adf59dc50b3534303e2fa8875abbc9f51b
Ajeet Singh <2016appsingh@gmail.com>
2022-12-02 15:19:13 +0530

    Chnages

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getData = async () =>{
-    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${text}`
+    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/black`
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data[0].meanings[0].definitions[0].definition);
